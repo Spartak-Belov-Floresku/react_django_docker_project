e33df8c87a93138b7e81a5f61716526f72c3173e
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2022-11-24 07:22:25 -0600

    add tests for reveiw

--- backend/core/tests/test_models.py
+++ backend/core/tests/test_models.py
@@ -13,6 +13,7 @@ from core import models
 def create_user(admin=False, **params):
     """Create and return a new user."""
     defaults = {
+        'first_name': 'testname',
         'username': 'test@mail.com',
         'email': 'test@mail.com',
         'password': 'password123',
@@ -24,9 +25,41 @@ def create_user(admin=False, **params):
     else:
         return User.objects.create_superuser(**defaults)
 
+def create_product(user):
+    """Create and return a product"""
+    product = models.Product.objects.create(
+        user=user,
+        name='Sample product name',
+        description='Sample product description',
+        price=Decimal('5.50')
+    )
+    return product
+
 
 class ModelTests(TestCase):
     """Test models."""
+
+    def test_craete_user_with_email_successful(self):
+        """Test creating a user wiyth an email is successful."""
+        email = 'test@mail.com'
+        password = 'testpass123'
+        user = create_user(email=email, password=password)
+
+        self.assertEquals(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_email = [
+            ['test1@MAIL.com', 'test1@mail.com'],
+            ['Test2@mail.com', 'Test2@mail.com'],
+            ['TEST3@MAIL.COM', 'TEST3@mail.com'],
+            ['test4@mail.COM', 'test4@mail.com'],
+        ]
+
+        for email, expected in sample_email:
+            user = create_user(username=expected, email=email)
+            self.assertEqual(user.email, expected)
 
     def test_create_product(self):
         """Test creating a product is successful."""
@@ -48,3 +81,19 @@ class ModelTests(TestCase):
         file_path = models.products_image_file_path(None, 'example.jpg')
 
         self.assertEqual(file_path, f'products/{uuid}.jpg')
+
+    def test_create_review(self):
+        """Test creating a product's review is successful."""
+        user = create_user()
+        product = create_product(user)
+        review = models.Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=5,
+            comment='test comment'
+        )
+        self.assertEqual(review.user, user)
+        self.assertEqual(review.product, product)
+        self.assertTrue(user.review_set.filter(product=product).exists())
+        self.assertTrue(product.review_set.filter(user=user).exists())
